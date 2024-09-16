19d34a2cebcca44e5c2ef6e43f4ec2d52e4a6019
BeTaJI <lilysh994@mail.ru>
2024-09-16 15:53:38 +0300

    удалил лишнее, сделал шапку и контентную часть с картинкой. Добавил prettier, ес линт и возможность импорта относительного через конфиг vite

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,81 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: "18.3" } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
+      ],
+      "sort-keys/sort-keys-fix": 1, // Сортировка свойств объектов в алфавитном порядке (asc)
+      "react/jsx-fragments": 0, // Разрешает использовать JSX фрагменты вместо `<div>`
+      "sort-destructure-keys/sort-destructure-keys": 2, // Сортировка параметров деструктуризации в алфавитном порядке (asc)
+      "react/button-has-type": 0, // Разрешает `<button>` без указания типа (button или submit)
+      "import/order": [
+        // Порядок импортов
+        "error", //
+        {
+          alphabetize: {
+            // Сортировка импортов в алфавитном порядке
+            caseInsensitive: true, // Игнорировать регистр
+            order: "asc", // Сортировка в алфавитном порядке
+          },
+          pathGroupsExcludedImportTypes: ["error", "react"],
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always", // Новые строки между импортами
+          pathGroups: [
+            // Группировка импортов
+            {
+              group: "external",
+              pattern: "react",
+              position: "before",
+            },
+            {
+              group: "internal",
+              pattern: "components/**",
+              position: "after",
+            },
+            {
+              group: "internal",
+              pattern: "pages/",
+              position: "after",
+            },
+          ],
+        },
       ],
     },
   },
-]
+];
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      assets: resolve(__dirname, './src/assets/'),
+      components: resolve(__dirname, './src/components/'),
+      consts: resolve(__dirname, './src/consts/'),
+      pages: resolve(__dirname, './src/pages/'),
+      store: resolve(__dirname, './src/store/'),
+      styles: resolve(__dirname, './src/styles/'),
+    },
+    extensions: ['.scss', '.js', '.jsx', '.ts', '.tsx'],
+  },
+});
