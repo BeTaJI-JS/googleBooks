535dbc96345d024b25f77ea3a0d87cce05aea15f
BeTaJI <lilysh994@mail.ru>
2024-09-17 12:12:41 +0300

    комопнет поиска, рабочий заапрос в поиске по апикею,  серчбар с имитацией фильтров по стилям

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,5 +1,35 @@
+import SearchBar from 'components/SearchBar';
 import styles from './styles.module.scss';
+import { useCallback, useState } from 'react';
+
+import axios from 'axios';
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+console.log('apiKey--->>>>>', apiKey);
 const MainPage = () => {
+  const [books, setBooks] = useState([]);
+  const [searchHistory, setSearchHistory] = useState([]);
+  console.log('books', books);
+  console.log('searchHistory', searchHistory);
+
+  const fetchBooks = useCallback(
+    async (query) => {
+      console.log('asdsa', query);
+
+      if (!query) return;
+
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`);
+      setBooks(response.data.items || []);
+
+      // Сохранение истории поиска
+      if (!searchHistory.includes(query)) {
+        setSearchHistory((prev) => [...prev, query]);
+      }
+    },
+    [searchHistory],
+  );
+
   return (
     <div>
       <div className={styles.title}>
@@ -19,6 +49,17 @@ const MainPage = () => {
             Наш веб-сайт предлагает всеобъемлющую и интуитивно понятную платформу для поиска, предоставляя читателям
             идеальные совпадения по их запросам Примите силу продвинутого поиска и отправляйтесь в путешествие
             литературного открытия
+          </div>
+        </div>
+      </div>
+      <div className={styles.searchBarContainer}>
+        <div className={styles.searchBar}>
+          <SearchBar onSearch={fetchBooks} history={searchHistory} />
+          <div className={styles.filterOptionsContainer}>
+            <div className={styles.filterContainer}>1 фильтр</div>
+            <div className={styles.filterContainer}>2тфильтр</div>
+            <div className={styles.filterContainer}>3фильтр</div>
+            <div className={styles.filterContainer}>4фильтр</div>
           </div>
         </div>
       </div>
