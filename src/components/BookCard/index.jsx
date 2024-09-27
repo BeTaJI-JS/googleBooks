246b5d52d43afc39db092c64c41d19028d1318e1
BeTaJI <lilysh994@mail.ru>
2024-09-27 14:31:18 +0300

    Откатил код назад... намудрил с виртуализацией. оставил фикс ошибки  в отсутсвии рефа для компонента испольщующего виртуализацию

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -6,23 +6,23 @@ import EmptyImage from 'assets/empty.svg';
 
 import styles from './styles.module.scss';
 
-const BookCard = ({ book }) => {
-  // const { title, authors, imageLinks, description } = book.volumeInfo;
+const BookCard = forwardRef(function BookCardBody({ book }, ref) {
+  const { title, authors, imageLinks, description } = book.volumeInfo;
 
   return (
     <Link to={`/book/${book.id}`} key={book.id}>
       <div className={styles.card}>
         <div>
-          <img src={book.volumeInfo.imageLinks?.thumbnail || EmptyImage} alt={book.volumeInfo.title} />
+          <img src={imageLinks?.thumbnail || EmptyImage} alt={title} />
         </div>
         <div className={styles.contentInfo}>
-          <div>{book.volumeInfo.authors?.join(', ') || 'Автор неизвестен'}</div>
-          <h3>{book.volumeInfo.title}</h3>
-          <div className={styles.description}>{book.volumeInfo.description}</div>
+          <div>{authors?.join(', ') || 'Автор неизвестен'}</div>
+          <h3>{title}</h3>
+          <div className={styles.description}>{description}</div>
         </div>
       </div>
     </Link>
   );
-};
+});
 
 export default BookCard;
