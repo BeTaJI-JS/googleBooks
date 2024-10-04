56562513996b01ef3b0009b6923482b8a51a02db
BeTaJI <lilysh994@mail.ru>
2024-10-04 11:18:51 +0300

    Добивал стилей к Подписи Найдено + цвета в шапке и оранжевый в colors

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -19,7 +19,10 @@ const CardContainer = ({ books, setPage, totalItems }) => {
 
   return (
     <div className={styles.wrapper}>
-      <div>Найдено: {totalItems} результатов</div>
+      <div>
+        <span>Найдено: </span>
+        {totalItems} результатов
+      </div>
       {books.length > 0 && (
         <VirtualList className={styles.virtualList} data={books} height={height} itemHeight={itemHeight} itemKey='etag'>
           {(book) => <BookCard book={book} />}
